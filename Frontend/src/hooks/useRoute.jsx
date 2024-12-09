c59df2653c8025ac35efc37fe96affa0a10a7814
Mrityunjay <mjguptacse@gmail.com>
2024-12-09 15:38:58 +0530

    First commit

---
+++ Frontend/src/hooks/useRoute.jsx
@@ -0,0 +1,49 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Doctors from "../pages/Doctors";
+import Login from "../pages/Login";
+import MyAppointments from "../pages/MyAppointments";
+import MyProfile from "../pages/MyProfile";
+import AppLayout from "../AppLayout";
+const useRoute = () => {
+  const appRoute = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/doctors",
+          element: <Doctors />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
+        },
+        {
+          path: "/myappointments",
+          element: <MyAppointments />,
+        },
+        {
+          path: "/profile",
+          element: <MyProfile />,
+        },
+      ],
+    },
+  ]);
+  return appRoute;
+};
+export default useRoute;
