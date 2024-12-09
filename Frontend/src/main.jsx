6cebefd9fb74803d6d93586fea41d56240514177
Mrityunjay <mjguptacse@gmail.com>
2024-12-09 23:06:07 +0530

    Navbar is done

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import useRoute from "./hooks/useRoute";
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const appRoute = useRoute();
