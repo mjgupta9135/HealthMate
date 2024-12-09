67ecb8c679bd6cbcba8492c8c7d0e71d946d2658
Mrityunjay <mjguptacse@gmail.com>
2024-12-10 00:13:57 +0530

    Speciality Menu Done

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Hero from "../components/Hero";
+import SpecialityMenu from "../components/SpecialityMenu";
 const Home = () => {
   return (
     <div>
       <Hero />
+      <SpecialityMenu />
     </div>
   );
 };
