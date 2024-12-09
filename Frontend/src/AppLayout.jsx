8952ba91d96a3e5e8e9e16d03b4669e04477cd95
Mrityunjay <mjguptacse@gmail.com>
2024-12-09 23:37:15 +0530

    Completed Hero Section

--- Frontend/src/AppLayout.jsx
+++ Frontend/src/AppLayout.jsx
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 const AppLayout = () => {
   return (
-    <div className="mx-4 sm:mx-[10%]">
+    <div className="mx-4 sm:mx-[5%]">
       <Header />
       <Outlet />
       <Footer />
