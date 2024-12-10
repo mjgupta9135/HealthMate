bcd49e0a8985d57f1f1b1b3cedeaf2f40f085372
Mrityunjay <mjguptacse@gmail.com>
2024-12-10 10:24:37 +0530

    Home Page Completed

--- Frontend/src/AppLayout.jsx
+++ Frontend/src/AppLayout.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import AppContextProvider from "./context/Appcontext";
 const AppLayout = () => {
   return (
-    <div className="mx-4 sm:mx-[5%]">
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+    <AppContextProvider>
+      <div className="mx-4 sm:mx-[5%]">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </AppContextProvider>
   );
 };
 
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Hero from "../components/Hero";
 import SpecialityMenu from "../components/SpecialityMenu";
 import TopDoctors from "../components/TopDoctors";
+import Banner from "../components/Banner";
 const Home = () => {
   return (
     <div>
       <Hero />
       <SpecialityMenu />
       <TopDoctors />
+      <Banner />
     </div>
   );
 };
