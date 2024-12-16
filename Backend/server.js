579afd9b3d0b80d38876f6b90c92df325646dbd2
Mrityunjay <mjguptacse@gmail.com>
2024-12-16 16:39:39 +0530

    Add Doctor API is Done

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongoDB.js";
 import connectCloudinary from "./config/cloudinary.js";
+import adminRouter from "./routes/adminRoute.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -15,6 +16,8 @@ app.use(express.json());
 app.use(cors());
 
 //API Endpoint
+
+app.use("/api/admin", adminRouter);
 app.get("/", (req, res) => {
   res.send("API Working");
 });
